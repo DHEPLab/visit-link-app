e00642df552472ff16c90ea54cf1cfc0bfc6a964
Hooks Zhang <hookszhang@gmail.com>
2020-04-15 12:12:43 +0800

    feat: layout HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,204 +1,101 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import {
-  Image,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React from 'react';
+import styled from 'styled-components';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Button, View, ScrollView } from 'react-native';
 
-import { MonoText } from '../components/StyledText';
-
-export default function HomeScreen() {
+export default function () {
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-      >
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>
-            Open up the code for this screen:
-          </Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          >
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will
-            automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
+    <SafeAreaView>
+      <ScrollView>
+        <Container>
+          <DateText>April 15, 2020</DateText>
+          <FlexContainer>
+            <NextVisit>
+              <NextVisitTitle>Your next visit is</NextVisitTitle>
+              <Card>
+                <CardTitle>Baby Info</CardTitle>
+                <View>
+                  <Row>
+                    <Label>Time</Label>
+                    <Value>10-11am</Value>
+                  </Row>
+                  <Row>
+                    <Label>Location</Label>
+                    <Value>吉林省延边朝鲜族自治州安图县朝阳街 836 号</Value>
+                  </Row>
+                </View>
+                <Button title="Start" />
+              </Card>
+            </NextVisit>
+            <VisitPlan>
+              <VisitPlanTitle>Visit plan for Jan 20th</VisitPlanTitle>
+            </VisitPlan>
+          </FlexContainer>
+        </Container>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}
-        >
-          <MonoText style={styles.codeHighlightText}>
-            navigation/BottomTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
-HomeScreen.navigationOptions = {
-  header: null,
-};
+const Container = styled.View``;
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
+const DateText = styled.Text`
+  padding-left: 12px;
+  font-size: 36px;
+  font-weight: bold;
+`;
 
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
+const FlexContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  height: 100%;
+  margin-top: 10px;
+  padding-bottom: 20px;
+`;
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
+const NextVisit = styled.View`
+  width: 40%;
+  margin-right: 10%;
+`;
 
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
-}
+const VisitPlan = styled.View`
+  width: 40%;
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
+const NextVisitTitle = styled.Text`
+  font-size: 40px;
+`;
+
+const VisitPlanTitle = styled.Text`
+  font-size: 28px;
+  font-weight: bold;
+`;
+
+const Card = styled.View`
+  margin-top: 20px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
+
+const CardTitle = styled.Text`
+  font-size: 18px;
+  margin: 20px auto;
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+  padding: 15px 10px;
+`;
+
+const Label = styled.Text`
+  color: #bbb;
+  font-size: 24px;
+  padding-right: 20px;
+`;
+
+const Value = styled.Text`
+  flex: 1;
+  text-align: right;
+  font-size: 18px;
+`;
