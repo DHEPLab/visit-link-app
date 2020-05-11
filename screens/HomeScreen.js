40909f9093a02f62fcf7db4f1eac8d56696a82cc
Hooks Zhang <hookszhang@gmail.com>
2020-05-11 12:42:36 +0800

    feat: layout home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,101 +1,66 @@
 import React from 'react';
-import styled from 'styled-components';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button, View, ScrollView } from 'react-native';
+import styled from 'styled-components/native';
+import { Dimensions, View, TouchableOpacity } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
+console.log(height, width);
 
 export default function () {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <Container>
-          <DateText>April 15, 2020</DateText>
-          <FlexContainer>
-            <NextVisit>
-              <NextVisitTitle>Your next visit is</NextVisitTitle>
-              <Card>
-                <CardTitle>Baby Info</CardTitle>
-                <View>
-                  <Row>
-                    <Label>Time</Label>
-                    <Value>10-11am</Value>
-                  </Row>
-                  <Row>
-                    <Label>Location</Label>
-                    <Value>吉林省延边朝鲜族自治州安图县朝阳街 836 号</Value>
-                  </Row>
-                </View>
-                <Button title="Start" />
-              </Card>
-            </NextVisit>
-            <VisitPlan>
-              <VisitPlanTitle>Visit plan for Jan 20th</VisitPlanTitle>
-            </VisitPlan>
-          </FlexContainer>
-        </Container>
-      </ScrollView>
-    </SafeAreaView>
+    <Container>
+      <Header>
+        <NextVisit>您的下一次家访：</NextVisit>
+      </Header>
+      <VisitCard></VisitCard>
+      <View>
+        {/* <TouchableOpacity> */}
+        <StartButton>开始课程</StartButton>
+        {/* </TouchableOpacity> */}
+      </View>
+    </Container>
   );
 }
 
-const Container = styled.View``;
-
-const DateText = styled.Text`
-  padding-left: 12px;
-  font-size: 36px;
-  font-weight: bold;
-`;
-
-const FlexContainer = styled.View`
-  flex-direction: row;
-  justify-content: center;
+const Container = styled.View`
   height: 100%;
-  margin-top: 10px;
-  padding-bottom: 20px;
 `;
 
-const NextVisit = styled.View`
-  width: 40%;
-  margin-right: 10%;
-`;
-
-const VisitPlan = styled.View`
-  width: 40%;
-`;
-
-const NextVisitTitle = styled.Text`
-  font-size: 40px;
-`;
-
-const VisitPlanTitle = styled.Text`
-  font-size: 28px;
+const NextVisit = styled.Text`
+  font-size: 20px;
+  color: #fff;
   font-weight: bold;
 `;
 
-const Card = styled.View`
-  margin-top: 20px;
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
+const VisitCard = styled.View`
+  align-self: center;
+  width: 308px;
+  height: 277px;
+  margin-top: -35px;
+  border-radius: 16px;
+  background-color: #fff;
+  elevation: 10;
+  z-index: 1;
 `;
 
-const CardTitle = styled.Text`
-  font-size: 18px;
-  margin: 20px auto;
+const Header = styled.View`
+  width: 100%;
+  height: 180px;
+  padding-top: 58px;
+  padding-left: 46px;
+  background: #ff9472;
 `;
 
-const Row = styled.View`
-  flex-direction: row;
-  padding: 15px 10px;
-`;
-
-const Label = styled.Text`
-  color: #bbb;
-  font-size: 24px;
-  padding-right: 20px;
-`;
-
-const Value = styled.Text`
-  flex: 1;
-  text-align: right;
-  font-size: 18px;
+const StartButton = styled.Text`
+  width: 260px;
+  height: 28px;
+  line-height: 28px;
+  background-color: #ff9472;
+  border-radius: 270px;
+  font-size: 12px;
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+  align-self: center;
+  margin-top: -14px;
+  elevation: 10;
 `;
