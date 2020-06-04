f727eeea72697f9c4b3b5baaef156e3e1602f8e4
Hooks Zhang <hookszhang@gmail.com>
2020-06-04 16:45:12 +0800

    fix: use formik wrapper form

--- screens/ChangePassword.js
+++ screens/ChangePassword.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { styled } from '../config/styled';
 import { Form, FormItem, Input, Button } from '../components/*';
+import { Formik } from 'formik';
 
 export default function () {
+  function onSubmit(values) {
+    console.log(values);
+  }
+
   return (
     <Container>
-      <Form>
-        <FormItem label="旧密码">
-          <Input secureTextEntry />
-        </FormItem>
-        <FormItem label="新密码">
-          <Input secureTextEntry />
-        </FormItem>
-        <FormItem label="确认密码" last>
-          <Input secureTextEntry />
-        </FormItem>
-      </Form>
-      <ButtonContainer>
-        <Button size="large" title="提交" />
-      </ButtonContainer>
+      <Formik initialValues={{}} onSubmit={onSubmit}>
+        {({ handleSubmit }) => (
+          <>
+            <Form>
+              <FormItem name="oldPassword" label="旧密码">
+                <Input secureTextEntry />
+              </FormItem>
+              <FormItem name="password" label="新密码">
+                <Input secureTextEntry />
+              </FormItem>
+              <FormItem name="confirmPassword" label="确认密码" noBorder>
+                <Input secureTextEntry />
+              </FormItem>
+            </Form>
+            <ButtonContainer>
+              <Button size="large" onPress={handleSubmit} title="提交" />
+            </ButtonContainer>
+          </>
+        )}
+      </Formik>
     </Container>
   );
 }
