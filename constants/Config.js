9b7afc8866e202ba8bd1235b049e3ed4bd140eb3
Hooks Zhang <hookszhang@gmail.com>
2020-06-08 17:28:28 +0800

    chore: update local api host

--- constants/Config.js
+++ constants/Config.js
@@ -2,7 +2,7 @@ import Constants from 'expo-constants';
 
 const environments = {
   local: {
-    apiHost: 'http://192.168.3.3',
+    apiHost: 'http://192.168.3.3:8080',
   },
   dev: {
     apiHost: 'https://dev.healthyfutures.cloud',
