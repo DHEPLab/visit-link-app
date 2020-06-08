651c011ec5dce7d07e15d3b446eaa81dde2ab4fc
Hooks Zhang <hookszhang@gmail.com>
2020-06-08 12:18:34 +0800

    build: pacakge files

--- ansible/package.sh
+++ ansible/package.sh
@@ -5,6 +5,6 @@ set -e -u
 root_dir="$(cd $(dirname $0)/../ && pwd)"
 echo 'Package files into tar file'
 cd "${root_dir}"
-tar --exclude "node_modules" -cvzf app.tar.gz "./"
+tar -cvzf app.tar.gz ".expo-shared" "__tests__" "assets" "components" "config" "constants" "navigation" "screens" "App.js" "app.json" "babel.config.js" "docker-compose.yml" "jsconfig.json" "package.json" "startDocker.sh" "yarn.lock"
 
 echo 'Package done.'
