f4368935c1cf046c413d2e1b1841836c74aa1eae
Hooks Zhang <hookszhang@gmail.com>
2020-06-10 11:35:10 +0800

    feat: layout babies screen, header

--- screens/Babies.js
+++ screens/Babies.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { TextInput } from 'react-native-gesture-handler';
 
-export default function () {
+import { styled } from '../utils/styled';
+import { Colors } from '../constants/*';
+import { Button } from '../components/*';
+
+export default function Babies() {
   return (
-    // <SafeAreaView style={{ paddingTop: Platform.OS === 'android' ? 25 : 0 }}>
-    <Text>Babies Screen</Text>
-    // </SafeAreaView>
+    <>
+      <Header {...Colors.linearGradient}>
+        <Search>
+          <TextInput placeholder="请您输入要搜索的宝宝姓名" />
+        </Search>
+      </Header>
+      <ListHeader>
+        <Title>宝宝列表</Title>
+        <Button title="添加宝宝" />
+      </ListHeader>
+    </>
   );
 }
+
+const ListHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 16px 28px;
+`;
+
+const Title = styled.Text`
+  font-size: 12px;
+  color: #525252;
+  font-weight: bold;
+`;
+
+const Header = styled(LinearGradient)`
+  width: 100%;
+  height: 56px;
+  padding-top: 22px;
+  align-items: center;
+`;
+
+const Search = styled.View`
+  width: 334px;
+  height: 24px;
+  background: rgba(0, 0, 0, 0.2);
+  justify-content: center;
+  border-radius: 12px;
+  padding-left: 12px;
+`;
