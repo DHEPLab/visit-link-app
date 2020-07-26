fc60167a3cb18c5128e56e518394b52b32064ebc
Hooks Zhang <hookszhang@gmail.com>
2020-07-26 21:07:44 +0800

    build: expo export, deploy docker nginx

--- ansible/build.sh
+++ ansible/build.sh
@@ -8,5 +8,6 @@ yarn config set registry https://registry.npm.taobao.org/
 yarn global add expo-cli
 yarn
 yarn test:ci
+yarn export:dev
 
 echo 'Build Done.'
--- ansible/package.sh
+++ ansible/package.sh
@@ -3,30 +3,9 @@
 set -e -u
 
 root_dir="$(cd $(dirname $0)/../ && pwd)"
-echo 'Package files into tar file'
 
+echo 'Package files into zip file'
 cd "${root_dir}"
-tar -cvzf app.tar.gz\
- ".expo-shared"\
- "__tests__"\
- "actions"\
- "assets"\
- "components"\
- "config"\
- "constants"\
- "navigation"\
- "reducers"\
- "screens"\
- "utils"\
- ".dockerignore"\
- "App.js"\
- "Dockerfile"\
- "app.json"\
- "babel.config.js"\
- "docker-compose.yml"\
- "jsconfig.json"\
- "package.json"\
- "startDocker.sh"\
- "yarn.lock" 
+tar --exclude -cvzf app.tar.gz "dist" "etc" "docker-compose.yml" "startDocker.sh"
 
 echo 'Package done.'
