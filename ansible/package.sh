7d2bd5d9c01cb9d0c9a6843970282236ca9cdea7
Hooks Zhang <hookszhang@gmail.com>
2020-07-26 21:16:22 +0800

    build: update package.sh, tar cmd

--- ansible/package.sh
+++ ansible/package.sh
@@ -6,6 +6,6 @@ root_dir="$(cd $(dirname $0)/../ && pwd)"
 
 echo 'Package files into zip file'
 cd "${root_dir}"
-tar --exclude -cvzf app.tar.gz "dist" "etc" "docker-compose.yml" "startDocker.sh"
+tar -cvzf app.tar.gz "dist" "etc" "docker-compose.yml" "startDocker.sh"
 
 echo 'Package done.'
