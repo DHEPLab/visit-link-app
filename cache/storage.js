c585c4c1788588a1cc94b02d4d5cd1423d767b80
Hooks Zhang <hookszhang@gmail.com>
2020-07-31 13:55:56 +0800

    refactor: cache storage hooks

--- cache/storage.js
+++ cache/storage.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 
-async function getObject(key) {
+function addObject(key, obj) {
+  return AsyncStorage.setItem(key, JSON.stringify(obj));
+}
+
+function getObject(key) {
   return new Promise(async (resolve, reject) => {
     try {
       const json = await AsyncStorage.getItem(key);
@@ -16,30 +20,32 @@ function getLesson(id) {
   return getObject(`LESSON_${id}`);
 }
 
+function getModule(id) {
+  return getObject(`MODULE_${id}`);
+}
+
+function getNextVisit() {
+  return getObject(`NEXT_VISIT`);
+}
+
+function use(getFn, id) {
+  const [value, setValue] = useState({});
+  useEffect(() => {
+    getFn(id).then((data) => {
+      setValue(data || {});
+    });
+  }, []);
+  return [value, setValue];
+}
+
 export default {
-  addLesson: (lesson) => {
-    return AsyncStorage.setItem(`LESSON_${lesson.id}`, JSON.stringify(lesson));
-  },
+  addLesson: (lesson) => addObject(`LESSON_${lesson.id}`, lesson),
+  addModule: (module) => addObject(`MODULE_${module.id}`, module),
+  setNextVisit: (visit) => addObject('NEXT_VISIT', visit),
   getLesson,
-  useLesson: (id) => {
-    const [value, setValue] = useState({});
-    useEffect(() => {
-      getLesson(id).then((data) => {
-        setValue(data || {});
-      });
-    }, []);
-    return [value, setValue];
-  },
-  addModule: (module) => {
-    return AsyncStorage.setItem(`MODULE_${module.id}`, JSON.stringify(module));
-  },
-  getModule: (id) => {
-    return getObject(`MODULE_${id}`);
-  },
-  setNextVisit: (visit) => {
-    return AsyncStorage.setItem('NEXT_VISIT', JSON.stringify(visit));
-  },
-  getNextVisit: () => {
-    return getObject(`NEXT_VISIT`);
-  },
+  getModule,
+  getNextVisit,
+  useLesson: (id) => use(getLesson, id),
+  useModule: (id) => use(getModule, id),
+  useNextVisit: () => use(getNextVisit),
 };
--- cache/storage.test.js
+++ cache/storage.test.js
@@ -1,18 +1,33 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { waitFor } from 'react-native-testing-library';
+
 import storage from './storage';
 
-it('should add lesson to storage', async () => {
+it('should add lesson to storage and useLesson', async () => {
   storage.addLesson({ id: 1, name: 'Lesson1' });
-  expect(await storage.getLesson(1)).toStrictEqual({ id: 1, name: 'Lesson1' });
+  const { result } = renderHook(() => storage.useLesson(1));
+  await waitFor(() => {
+    const [lesson] = result.current;
+    expect(lesson).toStrictEqual({ id: 1, name: 'Lesson1' });
+  });
 });
 
 it('should add module to storage', async () => {
   storage.addModule({ id: 2, name: 'Module1' });
-  expect(await storage.getModule(2)).toStrictEqual({ id: 2, name: 'Module1' });
+  const { result } = renderHook(() => storage.useModule(2));
+  await waitFor(() => {
+    const [module] = result.current;
+    expect(module).toStrictEqual({ id: 2, name: 'Module1' });
+  });
 });
 
 it('should set next visit to storage', async () => {
   storage.setNextVisit({ id: 3, name: 'Next Visit' });
-  expect(await storage.getNextVisit()).toStrictEqual({ id: 3, name: 'Next Visit' });
+  const { result } = renderHook(() => storage.useNextVisit());
+  await waitFor(() => {
+    const [visit] = result.current;
+    expect(visit).toStrictEqual({ id: 3, name: 'Next Visit' });
+  });
 });
 
 it('should return null', async () => {
