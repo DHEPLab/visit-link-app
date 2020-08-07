b5e4a33844bc2fee288c90e8550388517ac3d1dd
Hooks Zhang <hookszhang@gmail.com>
2020-08-07 11:02:30 +0800

    build: use offcial npm repository

--- ansible/build.sh
+++ ansible/build.sh
@@ -4,7 +4,7 @@ set -e -u
 
 echo 'Build with yarn'
 
-yarn config set registry https://registry.npm.taobao.org/
+# yarn config set registry https://registry.npm.taobao.org/
 yarn global add expo-cli
 yarn
 yarn test:ci
