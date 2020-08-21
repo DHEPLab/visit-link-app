0b4efc6a593ba3b4f83a23dc42581b2bd0adcb0a
Hooks Zhang <hookszhang@gmail.com>
2020-08-21 14:51:32 +0800

    feat(module): recursive page case components

--- utils/module.test.ts
+++ utils/module.test.ts
@@ -1,4 +1,4 @@
-import module, { Component } from './module';
+import module, { Component, Case, SwitchValue, TextValue } from './module';
 
 it('should create page components', () => {
   const components = [
@@ -14,5 +14,60 @@ it('should create page components', () => {
     [new Component('Text', 1596027303992), new Component('PageFooter', 1596420572527)],
     [new Component('Media', 1596027723356), new Component('PageFooter', 1596420574082)],
     [new Component('Text', 1596027758880), new Component('Switch', 1596419337285)],
+  ]);
+});
+
+it('should recursive page case components', () => {
+  const components = [
+    new Component(
+      'Switch',
+      1,
+      new SwitchValue(new TextValue(), [
+        new Case(
+          11,
+          'Level 1 Case 1',
+          ['Continue'],
+          [
+            new Component(
+              'Switch',
+              111,
+              new SwitchValue(new TextValue(), [new Case(1111, 'Level 2 Case 1', ['Continue'], [])])
+            ),
+            new Component('Text', 112),
+          ]
+        ),
+        new Case(
+          12,
+          'Level 1 Case 2',
+          ['Continue'],
+          [
+            new Component(
+              'Switch',
+              121,
+              new SwitchValue(new TextValue(), [new Case(1211, 'Level 2 Case 1', ['Continue'], [])])
+            ),
+            new Component('Text', 122),
+          ]
+        ),
+      ])
+    ),
+  ];
+
+  const case11 = new Case(11, 'Level 1 Case 1', ['Continue'], []);
+  const case1111 = new Case(1111, 'Level 2 Case 1', ['Continue'], []);
+  case1111.pageComponents = [];
+  case11.pageComponents = [
+    [new Component('Switch', 111, new SwitchValue(new TextValue(), [case1111]))],
+    [new Component('Text', 112)],
+  ];
+  const case12 = new Case(12, 'Level 1 Case 2', ['Continue'], []);
+  const case1211 = new Case(1211, 'Level 2 Case 1', ['Continue'], []);
+  case1211.pageComponents = [];
+  case12.pageComponents = [
+    [new Component('Switch', 121, new SwitchValue(new TextValue(), [case1211]))],
+    [new Component('Text', 122)],
+  ];
+  expect(module.pageable(components)).toStrictEqual([
+    [new Component('Switch', 1, new SwitchValue(new TextValue(), [case11, case12]))],
   ]);
 });
--- utils/module.ts
+++ utils/module.ts
@@ -1,47 +1,86 @@
 class ComponentValue {}
 
-class TextValue extends ComponentValue {
+export class TextValue extends ComponentValue {
   type: 'instruction' | 'script' | 'reference';
   html: string;
 }
 
-class MediaValue {
+export class MediaValue {
   type: 'VIDEO' | 'PHOTO';
   file: string;
   text: string;
 }
 
-class Case {
+export class Case {
   key: number;
   text: string;
-  finishAction: string[];
+  finishAction: ['Continue'] | ['Redirect_End', number] | ['Redirect_Continue', number];
   components: Component[];
+  pageComponents: Component[][];
+
+  constructor(
+    key: number,
+    text: string,
+    finishAction: ['Continue'] | ['Redirect_End', number] | ['Redirect_Continue', number],
+    components: Component[]
+  ) {
+    this.key = key;
+    this.text = text;
+    this.finishAction = finishAction;
+    this.components = components;
+  }
 }
 
-class SwitchValue {
+export class SwitchValue {
   question: TextValue;
   cases: Case[];
+
+  constructor(question: TextValue, cases: Case[]) {
+    this.question = question;
+    this.cases = cases;
+  }
 }
 
 export class Component {
   type: 'Text' | 'Media' | 'Switch' | 'PageFooter';
   key: number;
-  value: ComponentValue | null;
+  value?: ComponentValue;
 
-  constructor(type: 'Text' | 'Media' | 'Switch' | 'PageFooter', key: number) {
+  constructor(
+    type: 'Text' | 'Media' | 'Switch' | 'PageFooter',
+    key: number,
+    value?: ComponentValue
+  ) {
     this.type = type;
     this.key = key;
+    this.value = value;
+  }
+
+  isTypePageFooter() {
+    return 'PageFooter' === this.type;
+  }
+
+  isTypeSwitch() {
+    return 'Switch' === this.type;
   }
 }
 
-function pageable(components: Component[]) {
-  const page = [];
+function pageable(components: Component[]): Component[][] {
+  const page: Component[][] = [];
   let pageNumber = 0;
-  components.forEach((component) => {
+  components.forEach((component: Component) => {
     if (!page[pageNumber]) page[pageNumber] = [];
+    if (component.isTypeSwitch() && component.value) {
+      const switchValue = component.value as SwitchValue;
+      switchValue.cases.forEach((_case) => {
+        _case.pageComponents = pageable(_case.components);
+        _case.components = [];
+      });
+    }
+
     page[pageNumber].push(component);
 
-    if (component.type === 'PageFooter' || component.type === 'Switch') {
+    if (component.isTypePageFooter() || component.isTypeSwitch()) {
       pageNumber++;
     }
   });
