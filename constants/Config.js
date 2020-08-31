3643563aa368a3a59b02bbcd448030730ff6715b
Hooks Zhang <hookszhang@gmail.com>
2020-08-31 10:04:43 +0800

    feat: download file from api files

--- constants/Config.js
+++ constants/Config.js
@@ -4,15 +4,12 @@ const environments = {
   local: {
     apiHost: 'http://192.168.3.3:8080',
     // apiHost: 'http://192.168.43.30:8080',
-    ossHost: 'https://healthy-future-dev.oss-cn-shanghai.aliyuncs.com',
   },
   dev: {
     apiHost: 'https://dev.healthyfutures.cloud',
-    ossHost: 'https://healthy-future-dev.oss-cn-shanghai.aliyuncs.com',
   },
   stg: {
     apiHost: 'https://stg.healthyfutures.cloud',
-    ossHost: 'https://healthy-future-dev.oss-cn-shanghai.aliyuncs.com',
   },
 };
 
