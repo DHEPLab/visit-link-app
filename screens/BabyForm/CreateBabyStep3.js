dac3bdf7b90de3fcf2d763126b7ea5e7519e99b4
Hooks Zhang <hookszhang@gmail.com>
2020-09-01 10:57:31 +0800

    feat: add submit baby success message

--- screens/BabyForm/CreateBabyStep3.js
+++ screens/BabyForm/CreateBabyStep3.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { CommonActions } from '@react-navigation/native';
 
-import { CreateBabyNavigator, AddressForm } from '../../components';
+import { CreateBabyNavigator, AddressForm, Message } from '../../components';
 
 import http from '../../utils/http';
+import { useBoolState } from '../../utils';
 
 export default function CreateBabyStep3({ navigation, route }) {
   const { params } = route;
   const { baby, carers } = params;
+  const [visible, openMessage, closeMessage] = useBoolState();
 
   function onSubmit(values) {
     http
@@ -19,15 +21,19 @@ export default function CreateBabyStep3({ navigation, route }) {
         carers,
       })
       .then((data) => {
-        navigation.dispatch((state) => {
-          // clear all routing records except the home screen
-          const [home] = state.routes;
-          return CommonActions.reset({
-            index: 0,
-            routes: [home],
+        openMessage();
+        setTimeout(() => {
+          closeMessage();
+          navigation.dispatch((state) => {
+            // clear all routing records except the home screen
+            const [home] = state.routes;
+            return CommonActions.reset({
+              index: 0,
+              routes: [home],
+            });
           });
-        });
-        navigation.navigate('Baby', { ...data, tab: 'family' });
+          navigation.navigate('Baby', { ...data, tab: 'family' });
+        }, 1000);
       });
   }
 
@@ -35,6 +41,12 @@ export default function CreateBabyStep3({ navigation, route }) {
     <>
       <CreateBabyNavigator active={3} navigation={navigation} />
       <AddressForm onSubmit={onSubmit} />
+      <Message
+        visible={visible}
+        title="提交成功"
+        content="稍后可在列表中查看结果，新建宝宝需要督导员审核，如需尽快审核，请直接联系您的督导员。"
+        buttonText="知道了"
+      />
     </>
   );
 }
