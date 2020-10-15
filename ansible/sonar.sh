182f85912f190c818e5c697351ef9bf3115dd70a
Huey Zhang <hueyzng@gmail.com>
2020-10-15 17:57:35 +0800

    build: update sonar.sh, add typescript lcov report path

--- ansible/sonar.sh
+++ ansible/sonar.sh
@@ -4,4 +4,6 @@ sonar-scanner \
   -Dsonar.projectKey=healthy-future-app \
   -Dsonar.sources=. \
   -Dsonar.host.url=${SONAR_HOST_URL} \
-  -Dsonar.login=${SONAR_LOGIN}
+  -Dsonar.login=${SONAR_LOGIN} \
+  -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
+  -Dsonar.typescript.lcov.reportPaths=coverage/lcov.info
