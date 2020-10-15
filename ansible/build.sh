2698059c70b3916af55c2e3f98200673315983a8
Huey Zhang <hueyzng@gmail.com>
2020-10-15 13:03:34 +0800

    build: yarn global add sonarqube-scanner

--- ansible/build.sh
+++ ansible/build.sh
@@ -6,6 +6,7 @@ echo 'Build with yarn'
 
 yarn config set registry https://registry.npm.taobao.org/
 yarn global add expo-cli@3.23.1
+yarn global add sonarqube-scanner
 yarn
 yarn test:ci
 yarn export:dev
