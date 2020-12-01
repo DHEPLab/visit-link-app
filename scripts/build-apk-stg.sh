8e881c1bd645b672666a8e054ea8ab60a366a5e2
Zhang Hua <zhanghua@aidootech.com>
2020-12-01 02:57:52 +0000

    Revert "chore: remove unused code"
    This reverts commit b68b66898c859dae9b726b2e7a6dcf694e98dedb

---
+++ scripts/build-apk-stg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+EXPO_ANDROID_KEYSTORE_PASSWORD="keystorepassword" \
+EXPO_ANDROID_KEY_PASSWORD="keypassword" \
+turtle build:android \
+  --type apk \
+  --keystore-path ./keystore/dev.jks \
+  --keystore-alias "keyalias" \
+  --allow-non-https-public-url \
+  --public-url https://stg.healthyfutures.cloud/expo/android-index.json
