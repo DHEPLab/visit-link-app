246f23cd581298e034d967829b0eceb7e3aff24a
Tang Zhen <cloudztang@gmail.com>
2024-08-27 18:06:47 +0800

    [HF-42][Zhen Tang]chore: remove useless files

--- ansible/sonar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sonar-scanner \
-  -Dsonar.projectKey=healthy-future-app \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=${SONAR_HOST_URL} \
-  -Dsonar.login=${SONAR_LOGIN} \
-  -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
-  -Dsonar.typescript.lcov.reportPaths=coverage/lcov.info
