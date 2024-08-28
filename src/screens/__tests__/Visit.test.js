8863dd3a11c7e2380280c0e9893242b825913ead
murrayee <murrayee@163.com>
2024-08-28 10:13:50 +0800

    [HF-16][JIAXIN]chore: fix test

--- src/screens/__tests__/Visit.test.js
+++ src/screens/__tests__/Visit.test.js
@@ -62,7 +62,7 @@ describe("<Visit />", () => {
     const { queryByText, queryAllByText } = render(
       <Visit {...createTestProps()} />,
     );
-    expect(queryAllByText(/未完成原因/).length).toBe(1);
+    expect(queryAllByText(/Visits:undoneReason/).length).toBe(1);
     expect(queryByText(/继续课堂/)).not.toBeNull();
   });
 
@@ -73,7 +73,7 @@ describe("<Visit />", () => {
       },
     ]);
     const { queryAllByText } = render(<Visit {...createTestProps()} />);
-    expect(queryAllByText(/过期原因/).length).toBe(1);
+    expect(queryAllByText(/Visits:undoneReason/).length).toBe(1);
   });
 
   it("should readonly", () => {
