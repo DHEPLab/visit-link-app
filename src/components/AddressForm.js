5cd6e7ab39854495ee6fe2749e64d06b1594234c
Yuheng Zhou <yuheng.zhou@thoughtworks.com>
2024-09-20 15:39:43 +0800

    [HF-95][Yuheng Zhou] feat: remove log

--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -46,7 +46,6 @@ export default function AddressForm({
   const autoCompleteRef = useRef();
 
   useEffect(() => {
-    console.log(initialValues);
     autoCompleteRef.current?.setAddressText(initialValues.area);
   }, []);
 
